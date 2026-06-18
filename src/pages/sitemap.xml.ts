d97af353041b3a6d7ce91e11eccdd275c44d61c8
String020 <fiftystring02@gmail.com>
2026-06-18 18:52:47 +0800

    Include writing guide and series pages in sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,5 +1,5 @@
 import { getCollection } from "astro:content";
-import { getAllCategories, getAllTags, isPublished, slugifyTaxonomy, sortPosts } from "../lib/blog";
+import { getAllCategories, getAllSeries, getAllTags, isPublished, slugifyTaxonomy, sortPosts } from "../lib/blog";
 
 const escapeXml = (value: string) =>
   value
@@ -18,15 +18,18 @@ export async function GET({ site }: { site?: URL }) {
   const posts = sortPosts((await getCollection("blog")).filter(isPublished));
   const tags = getAllTags(posts);
   const categories = getAllCategories(posts);
+  const seriesList = getAllSeries(posts);
 
   const paths = [
     "/",
     "/blog/",
     "/blog/archive/",
+    "/blog/writing-guide/",
     "/search/",
     ...posts.map((post) => `/blog/${post.id}/`),
     ...tags.map((tag) => `/blog/tag/${slugifyTaxonomy(tag)}/`),
     ...categories.map((category) => `/blog/category/${slugifyTaxonomy(category)}/`),
+    ...seriesList.map((series) => `/blog/series/${slugifyTaxonomy(series)}/`),
   ];
 
   const urls = paths
